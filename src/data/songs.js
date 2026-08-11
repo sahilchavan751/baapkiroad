ae98dadbdb73d711e47c24080801cdef6e2216ad
BigSmoke76-cyber <baviskarpratik76@gmail.com>
2026-08-11 16:43:43 +0530

    goooo

--- src/data/songs.js
+++ src/data/songs.js
@@ -1,32 +1,436 @@
 export const SONGS = [
-  { id: 1, title: "Mujhse Mohabbat Ka Izhaar", artist: "Shemaroo Filmi Gaane", videoId: "N0jnLZxYwYc", cover: "https://img.youtube.com/vi/N0jnLZxYwYc/mqdefault.jpg" },
-  { id: 2, title: "Tumsa Koi Pyaara", artist: "Kumar Sanu, Alka Yagnik", videoId: "3NWMK2MRqIk", cover: "https://img.youtube.com/vi/3NWMK2MRqIk/mqdefault.jpg" },
-  { id: 3, title: "Woh Meri Neend Mera Chain", artist: "Sadhana Sargam", videoId: "bga_0ziOOfQ", cover: "https://img.youtube.com/vi/bga_0ziOOfQ/mqdefault.jpg" },
-  { id: 4, title: "Saaton Janam Main Tere", artist: "Kumar Sanu", videoId: "oFxbBeYhLqM", cover: "https://img.youtube.com/vi/oFxbBeYhLqM/mqdefault.jpg" },
-  { id: 5, title: "Oye Raju Pyar Na Kariyo", artist: "Anand Raj Anand", videoId: "nNhv8A_rJTg", cover: "https://img.youtube.com/vi/nNhv8A_rJTg/mqdefault.jpg" },
-  { id: 6, title: "Bahut Pyar Karte Hain", artist: "Saajan", videoId: "d3lZvNexPL0", cover: "https://img.youtube.com/vi/d3lZvNexPL0/mqdefault.jpg" },
-  { id: 7, title: "Jeeta Tha Jiske Liye", artist: "Kumar Sanu", videoId: "CTuvMubzXpU", cover: "https://img.youtube.com/vi/CTuvMubzXpU/mqdefault.jpg" },
-  { id: 8, title: "Teri Umeed Tera Intezar", artist: "Kumar Sanu, Sadhana Sargam", videoId: "i1IsLVz6T9Q", cover: "https://img.youtube.com/vi/i1IsLVz6T9Q/mqdefault.jpg" },
-  { id: 9, title: "Tumse Milne Ko Dil Karta Hai", artist: "Kumar Sanu, Alka Yagnik", videoId: "5y_TCKNzAMI", cover: "https://img.youtube.com/vi/5y_TCKNzAMI/mqdefault.jpg" },
-  { id: 10, title: "Bas Ek Sanam Chahiye", artist: "Kumar Sanu", videoId: "fBylcT-TWZw", cover: "https://img.youtube.com/vi/fBylcT-TWZw/mqdefault.jpg" },
-  { id: 11, title: "Tu Pyar Hai Kisi Aur Ka", artist: "Babla Mehta, Anuradha Paudwal", videoId: "CTNgz5gb3D8", cover: "https://img.youtube.com/vi/CTNgz5gb3D8/mqdefault.jpg" },
-  { id: 12, title: "Sochenge Tumhe Pyar", artist: "Kumar Sanu", videoId: "lFdSi01tpYM", cover: "https://img.youtube.com/vi/lFdSi01tpYM/mqdefault.jpg" },
-  { id: 13, title: "Raah Mein Unse Mulaqat", artist: "Kumar Sanu, Alka Yagnik", videoId: "dDR4oiyjUBA", cover: "https://img.youtube.com/vi/dDR4oiyjUBA/mqdefault.jpg" },
-  { id: 14, title: "Main Duniya Bhula Doonga", artist: "Kumar Sanu", videoId: "otQmzlm-s7Q", cover: "https://img.youtube.com/vi/otQmzlm-s7Q/mqdefault.jpg" },
-  { id: 15, title: "Tumhein Apna Banane Ki Kasam", artist: "Kumar Sanu, Anuradha Paudwal", videoId: "tPNwGuu_rQ4", cover: "https://img.youtube.com/vi/tPNwGuu_rQ4/mqdefault.jpg" },
-  { id: 16, title: "Tum Dil Ki Dhadkan Mein Rehte Ho", artist: "Abhijeet Bhattacharya", videoId: "p1jhKCIoVjI", cover: "https://img.youtube.com/vi/p1jhKCIoVjI/mqdefault.jpg" },
-  { id: 17, title: "Dulhe Ka Sehra Suhana", artist: "Nusrat Fateh Ali Khan", videoId: "2OsyNo53MzU", cover: "https://img.youtube.com/vi/2OsyNo53MzU/mqdefault.jpg" },
-  { id: 18, title: "Maine Pyaar Tumhi Se Kiya Hai", artist: "Anuradha Paudwal, Kumar Sanu", videoId: "-N-k56i7M2k", cover: "https://img.youtube.com/vi/-N-k56i7M2k/mqdefault.jpg" },
-  { id: 19, title: "Ab Tere Bin Jee Lenge Hum", artist: "Kumar Sanu", videoId: "rXHY4Cv9cA8", cover: "https://img.youtube.com/vi/rXHY4Cv9cA8/mqdefault.jpg" },
-  { id: 20, title: "Kitna Haseen Chehra", artist: "Kumar Sanu", videoId: "qGOTe3KmCdY", cover: "https://img.youtube.com/vi/qGOTe3KmCdY/mqdefault.jpg" },
-  { id: 21, title: "Tujhko Na Dekhu Toh", artist: "Sunidhi Chauhan, Udit Narayan", videoId: "cGKBs7rokos", cover: "https://img.youtube.com/vi/cGKBs7rokos/mqdefault.jpg" },
-  { id: 22, title: "Chand Tare Phool", artist: "Tum Se Achcha Kaun Hai", videoId: "BtdiNnrftYM", cover: "https://img.youtube.com/vi/BtdiNnrftYM/mqdefault.jpg" },
-  { id: 23, title: "Tum Dil Ki Dhadkan Mein", artist: "Kumar Sanu", videoId: "nRJ8vHpi6_g", cover: "https://img.youtube.com/vi/nRJ8vHpi6_g/mqdefault.jpg" },
-  { id: 24, title: "Sab Kuchh Bhula Diya", artist: "Sonu Nigam, Sapna Awasthi", videoId: "xKx_80QM2LU", cover: "https://img.youtube.com/vi/xKx_80QM2LU/mqdefault.jpg" },
-  { id: 25, title: "Dheere Dheere Pyar Ko Badhana Hai", artist: "Kumar Sanu, Alka Yagnik", videoId: "zuPoUsdXrqM", cover: "https://img.youtube.com/vi/zuPoUsdXrqM/mqdefault.jpg" },
-  { id: 26, title: "Jeeye To Jeeye Kaise", artist: "Pankaj Udhas", videoId: "wYdXuNtJkPk", cover: "https://img.youtube.com/vi/wYdXuNtJkPk/mqdefault.jpg" },
-  { id: 27, title: "Hum Pyaar Hai Tumhare", artist: "Kumar Sanu", videoId: "wuLJtA0uJro", cover: "https://img.youtube.com/vi/wuLJtA0uJro/mqdefault.jpg" },
-  { id: 28, title: "Nahin Yeh Ho Nahin Sakta", artist: "Kumar Sanu, Sadhana Sargam", videoId: "RjJxWRFfG3s", cover: "https://img.youtube.com/vi/RjJxWRFfG3s/mqdefault.jpg" },
-  { id: 29, title: "Kitna Pyaara Tujhe Rabne Banaya", artist: "Alka Yagnik, Udit Narayan", videoId: "wV8njoRVefQ", cover: "https://img.youtube.com/vi/wV8njoRVefQ/mqdefault.jpg" },
-  { id: 30, title: "Hum Pyaar Hai Tumhare", artist: "Kumar Sanu", videoId: "4ImdbyqnH8w", cover: "https://img.youtube.com/vi/4ImdbyqnH8w/mqdefault.jpg" },
+  {
+    "id": 1,
+    "title": "Mujhse Mohabbat Ka Izhaar",
+    "artist": "Shemaroo Filmi Gaane",
+    "videoId": "N0jnLZxYwYc",
+    "cover": "https://img.youtube.com/vi/N0jnLZxYwYc/mqdefault.jpg"
+  },
+  {
+    "id": 2,
+    "title": "Tumsa Koi Pyaara",
+    "artist": "Kumar Sanu, Alka Yagnik",
+    "videoId": "3NWMK2MRqIk",
+    "cover": "https://img.youtube.com/vi/3NWMK2MRqIk/mqdefault.jpg"
+  },
+  {
+    "id": 3,
+    "title": "Woh Meri Neend Mera Chain",
+    "artist": "Sadhana Sargam",
+    "videoId": "bga_0ziOOfQ",
+    "cover": "https://img.youtube.com/vi/bga_0ziOOfQ/mqdefault.jpg"
+  },
+  {
+    "id": 4,
+    "title": "Saaton Janam Main Tere",
+    "artist": "Kumar Sanu",
+    "videoId": "oFxbBeYhLqM",
+    "cover": "https://img.youtube.com/vi/oFxbBeYhLqM/mqdefault.jpg"
+  },
+  {
+    "id": 5,
+    "title": "Oye Raju Pyar Na Kariyo",
+    "artist": "Anand Raj Anand",
+    "videoId": "nNhv8A_rJTg",
+    "cover": "https://img.youtube.com/vi/nNhv8A_rJTg/mqdefault.jpg"
+  },
+  {
+    "id": 6,
+    "title": "Bahut Pyar Karte Hain",
+    "artist": "Saajan",
+    "videoId": "d3lZvNexPL0",
+    "cover": "https://img.youtube.com/vi/d3lZvNexPL0/mqdefault.jpg"
+  },
+  {
+    "id": 7,
+    "title": "Jeeta Tha Jiske Liye",
+    "artist": "Kumar Sanu",
+    "videoId": "CTuvMubzXpU",
+    "cover": "https://img.youtube.com/vi/CTuvMubzXpU/mqdefault.jpg"
+  },
+  {
+    "id": 8,
+    "title": "Teri Umeed Tera Intezar",
+    "artist": "Kumar Sanu, Sadhana Sargam",
+    "videoId": "i1IsLVz6T9Q",
+    "cover": "https://img.youtube.com/vi/i1IsLVz6T9Q/mqdefault.jpg"
+  },
+  {
+    "id": 9,
+    "title": "Tumse Milne Ko Dil Karta Hai",
+    "artist": "Kumar Sanu, Alka Yagnik",
+    "videoId": "5y_TCKNzAMI",
+    "cover": "https://img.youtube.com/vi/5y_TCKNzAMI/mqdefault.jpg"
+  },
+  {
+    "id": 10,
+    "title": "Bas Ek Sanam Chahiye",
+    "artist": "Kumar Sanu",
+    "videoId": "fBylcT-TWZw",
+    "cover": "https://img.youtube.com/vi/fBylcT-TWZw/mqdefault.jpg"
+  },
+  {
+    "id": 11,
+    "title": "Tu Pyar Hai Kisi Aur Ka",
+    "artist": "Babla Mehta, Anuradha Paudwal",
+    "videoId": "CTNgz5gb3D8",
+    "cover": "https://img.youtube.com/vi/CTNgz5gb3D8/mqdefault.jpg"
+  },
+  {
+    "id": 12,
+    "title": "Sochenge Tumhe Pyar",
+    "artist": "Kumar Sanu",
+    "videoId": "lFdSi01tpYM",
+    "cover": "https://img.youtube.com/vi/lFdSi01tpYM/mqdefault.jpg"
+  },
+  {
+    "id": 13,
+    "title": "Raah Mein Unse Mulaqat",
+    "artist": "Kumar Sanu, Alka Yagnik",
+    "videoId": "dDR4oiyjUBA",
+    "cover": "https://img.youtube.com/vi/dDR4oiyjUBA/mqdefault.jpg"
+  },
+  {
+    "id": 14,
+    "title": "Main Duniya Bhula Doonga",
+    "artist": "Kumar Sanu",
+    "videoId": "otQmzlm-s7Q",
+    "cover": "https://img.youtube.com/vi/otQmzlm-s7Q/mqdefault.jpg"
+  },
+  {
+    "id": 15,
+    "title": "Tumhein Apna Banane Ki Kasam",
+    "artist": "Kumar Sanu, Anuradha Paudwal",
+    "videoId": "tPNwGuu_rQ4",
+    "cover": "https://img.youtube.com/vi/tPNwGuu_rQ4/mqdefault.jpg"
+  },
+  {
+    "id": 16,
+    "title": "Tum Dil Ki Dhadkan Mein Rehte Ho",
+    "artist": "Abhijeet Bhattacharya",
+    "videoId": "p1jhKCIoVjI",
+    "cover": "https://img.youtube.com/vi/p1jhKCIoVjI/mqdefault.jpg"
+  },
+  {
+    "id": 17,
+    "title": "Dulhe Ka Sehra Suhana",
+    "artist": "Nusrat Fateh Ali Khan",
+    "videoId": "2OsyNo53MzU",
+    "cover": "https://img.youtube.com/vi/2OsyNo53MzU/mqdefault.jpg"
+  },
+  {
+    "id": 18,
+    "title": "Maine Pyaar Tumhi Se Kiya Hai",
+    "artist": "Anuradha Paudwal, Kumar Sanu",
+    "videoId": "-N-k56i7M2k",
+    "cover": "https://img.youtube.com/vi/-N-k56i7M2k/mqdefault.jpg"
+  },
+  {
+    "id": 19,
+    "title": "Ab Tere Bin Jee Lenge Hum",
+    "artist": "Kumar Sanu",
+    "videoId": "rXHY4Cv9cA8",
+    "cover": "https://img.youtube.com/vi/rXHY4Cv9cA8/mqdefault.jpg"
+  },
+  {
+    "id": 20,
+    "title": "Kitna Haseen Chehra",
+    "artist": "Kumar Sanu",
+    "videoId": "qGOTe3KmCdY",
+    "cover": "https://img.youtube.com/vi/qGOTe3KmCdY/mqdefault.jpg"
+  },
+  {
+    "id": 21,
+    "title": "Tujhko Na Dekhu Toh",
+    "artist": "Sunidhi Chauhan, Udit Narayan",
+    "videoId": "cGKBs7rokos",
+    "cover": "https://img.youtube.com/vi/cGKBs7rokos/mqdefault.jpg"
+  },
+  {
+    "id": 22,
+    "title": "Chand Tare Phool",
+    "artist": "Tum Se Achcha Kaun Hai",
+    "videoId": "BtdiNnrftYM",
+    "cover": "https://img.youtube.com/vi/BtdiNnrftYM/mqdefault.jpg"
+  },
+  {
+    "id": 23,
+    "title": "Tum Dil Ki Dhadkan Mein",
+    "artist": "Kumar Sanu",
+    "videoId": "nRJ8vHpi6_g",
+    "cover": "https://img.youtube.com/vi/nRJ8vHpi6_g/mqdefault.jpg"
+  },
+  {
+    "id": 24,
+    "title": "Sab Kuchh Bhula Diya",
+    "artist": "Sonu Nigam, Sapna Awasthi",
+    "videoId": "xKx_80QM2LU",
+    "cover": "https://img.youtube.com/vi/xKx_80QM2LU/mqdefault.jpg"
+  },
+  {
+    "id": 25,
+    "title": "Dheere Dheere Pyar Ko Badhana Hai",
+    "artist": "Kumar Sanu, Alka Yagnik",
+    "videoId": "zuPoUsdXrqM",
+    "cover": "https://img.youtube.com/vi/zuPoUsdXrqM/mqdefault.jpg"
+  },
+  {
+    "id": 26,
+    "title": "Jeeye To Jeeye Kaise",
+    "artist": "Pankaj Udhas",
+    "videoId": "wYdXuNtJkPk",
+    "cover": "https://img.youtube.com/vi/wYdXuNtJkPk/mqdefault.jpg"
+  },
+  {
+    "id": 27,
+    "title": "Hum Pyaar Hai Tumhare",
+    "artist": "Kumar Sanu",
+    "videoId": "wuLJtA0uJro",
+    "cover": "https://img.youtube.com/vi/wuLJtA0uJro/mqdefault.jpg"
+  },
+  {
+    "id": 28,
+    "title": "Nahin Yeh Ho Nahin Sakta",
+    "artist": "Kumar Sanu, Sadhana Sargam",
+    "videoId": "RjJxWRFfG3s",
+    "cover": "https://img.youtube.com/vi/RjJxWRFfG3s/mqdefault.jpg"
+  },
+  {
+    "id": 29,
+    "title": "Kitna Pyaara Tujhe Rabne Banaya",
+    "artist": "Alka Yagnik, Udit Narayan",
+    "videoId": "wV8njoRVefQ",
+    "cover": "https://img.youtube.com/vi/wV8njoRVefQ/mqdefault.jpg"
+  },
+  {
+    "id": 30,
+    "title": "Hum Pyaar Hai Tumhare",
+    "artist": "Kumar Sanu",
+    "videoId": "4ImdbyqnH8w",
+    "cover": "https://img.youtube.com/vi/4ImdbyqnH8w/mqdefault.jpg"
+  },
+  {
+    "id": 31,
+    "title": "Ek Ladki Ko Dekha To Aisa Laga",
+    "artist": "Kumar Sanu",
+    "videoId": "htMvfOfixuM",
+    "cover": "https://img.youtube.com/vi/htMvfOfixuM/mqdefault.jpg"
+  },
+  {
+    "id": 32,
+    "title": "Tere Dar Par Sanam",
+    "artist": "Kumar Sanu",
+    "videoId": "5dWbn_qER3s",
+    "cover": "https://img.youtube.com/vi/5dWbn_qER3s/mqdefault.jpg"
+  },
+  {
+    "id": 33,
+    "title": "Chura Ke Dil Mera",
+    "artist": "Kumar Sanu, Alka Yagnik",
+    "videoId": "6Na7GSV9bVY",
+    "cover": "https://img.youtube.com/vi/6Na7GSV9bVY/mqdefault.jpg"
+  },
+  {
+    "id": 34,
+    "title": "Tu Meri Zindagi Hai",
+    "artist": "Kumar Sanu, Anuradha Paudwal",
+    "videoId": "oEg_iXEWlt4",
+    "cover": "https://img.youtube.com/vi/oEg_iXEWlt4/mqdefault.jpg"
+  },
+  {
+    "id": 35,
+    "title": "Chori Chori Dil Tera",
+    "artist": "Kumar Sanu, Sujata Goswandy",
+    "videoId": "QjqKXFGM3eI",
+    "cover": "https://img.youtube.com/vi/QjqKXFGM3eI/mqdefault.jpg"
+  },
+  {
+    "id": 36,
+    "title": "Ek Aisi Ladki Thi Jise Mai Pyar Karta Tha",
+    "artist": "Kumar Sanu, Alka Yagnik",
+    "videoId": "Dz1Ad3cdtQA",
+    "cover": "https://img.youtube.com/vi/Dz1Ad3cdtQA/mqdefault.jpg"
+  },
+  {
+    "id": 37,
+    "title": "Achha Sila Diya Toone Mere Pyar Ka",
+    "artist": "Sonu Nigam",
+    "videoId": "G7AdjVDBLO8",
+    "cover": "https://img.youtube.com/vi/G7AdjVDBLO8/mqdefault.jpg"
+  },
+  {
+    "id": 38,
+    "title": "Tere Dard Se Dil",
+    "artist": "Kumar Sanu",
+    "videoId": "TgHYW8ubFko",
+    "cover": "https://img.youtube.com/vi/TgHYW8ubFko/mqdefault.jpg"
+  },
+  {
+    "id": 39,
+    "title": "Hum Yaar Hai Tumhare",
+    "artist": "Alka Yagnik, Udit Narayan",
+    "videoId": "uIOrAkrjwp4",
+    "cover": "https://img.youtube.com/vi/uIOrAkrjwp4/mqdefault.jpg"
+  },
+  {
+    "id": 40,
+    "title": "Aitbaar Nahi Karna",
+    "artist": "Abhijeet Bhattacharya, Sadhana Sargam",
+    "videoId": "HoMSu1iw0Zw",
+    "cover": "https://img.youtube.com/vi/HoMSu1iw0Zw/mqdefault.jpg"
+  },
+  {
+    "id": 41,
+    "title": "Dekha Hai Pehli Baar",
+    "artist": "Alka Yagnik, SP Balasubrahmanyam",
+    "videoId": "WAgJ8KM5AVQ",
+    "cover": "https://img.youtube.com/vi/WAgJ8KM5AVQ/mqdefault.jpg"
+  },
+  {
+    "id": 42,
+    "title": "Aankh Hai Bhari Bhari",
+    "artist": "Kumar Sanu",
+    "videoId": "OgocnLh9P1M",
+    "cover": "https://img.youtube.com/vi/OgocnLh9P1M/mqdefault.jpg"
+  },
+  {
+    "id": 43,
+    "title": "Kya Karte They Sajna",
+    "artist": "Anuradha Paudwal, Udit Narayan",
+    "videoId": "Zi9UBJQMz3I",
+    "cover": "https://img.youtube.com/vi/Zi9UBJQMz3I/mqdefault.jpg"
+  },
+  {
+    "id": 44,
+    "title": "Dekhne Waalon Ne",
+    "artist": "Udit Narayan, Alka Yagnik",
+    "videoId": "_dUAVM5ERXA",
+    "cover": "https://img.youtube.com/vi/_dUAVM5ERXA/mqdefault.jpg"
+  },
+  {
+    "id": 45,
+    "title": "Tum To Thehre Pardesi",
+    "artist": "Altaf Raja",
+    "videoId": "lRBIcaSV-Ns",
+    "cover": "https://img.youtube.com/vi/lRBIcaSV-Ns/mqdefault.jpg"
+  },
+  {
+    "id": 46,
+    "title": "Chehra Kya Dekhte Ho",
+    "artist": "Kumar Sanu, Asha Bhosle",
+    "videoId": "9v2bq2JHt4I",
+    "cover": "https://img.youtube.com/vi/9v2bq2JHt4I/mqdefault.jpg"
+  },
+  {
+    "id": 47,
+    "title": "Too Shayar Hai Main Teri Shayari",
+    "artist": "Alka Yagnik",
+    "videoId": "Gg9ZUppafLo",
+    "cover": "https://img.youtube.com/vi/Gg9ZUppafLo/mqdefault.jpg"
+  },
+  {
+    "id": 48,
+    "title": "Paas Woh Aane Lage",
+    "artist": "Kumar Sanu, Alka Yagnik",
+    "videoId": "w89fWEelFns",
+    "cover": "https://img.youtube.com/vi/w89fWEelFns/mqdefault.jpg"
+  },
+  {
+    "id": 49,
+    "title": "Chhupana Bhi Nahin Aata",
+    "artist": "Vinod Rathod",
+    "videoId": "fg9G1dacXjk",
+    "cover": "https://img.youtube.com/vi/fg9G1dacXjk/mqdefault.jpg"
+  },
+  {
+    "id": 50,
+    "title": "Is Tarah Aashiqui Ka",
+    "artist": "Kumar Sanu",
+    "videoId": "Y-o8NQ8Y36A",
+    "cover": "https://img.youtube.com/vi/Y-o8NQ8Y36A/mqdefault.jpg"
+  },
+  {
+    "id": 51,
+    "title": "Tumse Milna",
+    "artist": "Udit Narayan, Alka Yagnik",
+    "videoId": "526hvVlBP1U",
+    "cover": "https://img.youtube.com/vi/526hvVlBP1U/mqdefault.jpg"
+  },
+  {
+    "id": 52,
+    "title": "Kyo Kisi Ko",
+    "artist": "Udit Narayan",
+    "videoId": "iCZfjggJg3M",
+    "cover": "https://img.youtube.com/vi/iCZfjggJg3M/mqdefault.jpg"
+  },
+  {
+    "id": 53,
+    "title": "Dil Ka Aalam",
+    "artist": "Kumar Sanu",
+    "videoId": "BaAoZA0fup0",
+    "cover": "https://img.youtube.com/vi/BaAoZA0fup0/mqdefault.jpg"
+  },
+  {
+    "id": 54,
+    "title": "Pehli Pehli Baar Mohabbat Ki Hai",
+    "artist": "Kumar Sanu, Alka Yagnik",
+    "videoId": "cBGDDBHN22U",
+    "cover": "https://img.youtube.com/vi/cBGDDBHN22U/mqdefault.jpg"
+  },
+  {
+    "id": 55,
+    "title": "Tune Dil Mera Toda",
+    "artist": "Lata Mangeshkar",
+    "videoId": "nG85YFR3o6U",
+    "cover": "https://img.youtube.com/vi/nG85YFR3o6U/mqdefault.jpg"
+  },
+  {
+    "id": 56,
+    "title": "Kaash Kahin Aisa Hota",
+    "artist": "Kumar Sanu",
+    "videoId": "TRUuSFW80Rk",
+    "cover": "https://img.youtube.com/vi/TRUuSFW80Rk/mqdefault.jpg"
+  },
+  {
+    "id": 57,
+    "title": "Aawara Hawa Ka Jhonka Hoon",
+    "artist": "Altaf Raja",
+    "videoId": "-pIMyf5dOnA",
+    "cover": "https://img.youtube.com/vi/-pIMyf5dOnA/mqdefault.jpg"
+  },
+  {
+    "id": 58,
+    "title": "Love Tujhe Love Main Karta Hoon",
+    "artist": "Kumar Sanu, Alka Yagnik",
+    "videoId": "GxaTSDnI71w",
+    "cover": "https://img.youtube.com/vi/GxaTSDnI71w/mqdefault.jpg"
+  },
+  {
+    "id": 59,
+    "title": "Tere Dard Se Dil Aabad Raha",
+    "artist": "Deewana Movie",
+    "videoId": "XWKazQwFFdY",
+    "cover": "https://img.youtube.com/vi/XWKazQwFFdY/mqdefault.jpg"
+  },
+  {
+    "id": 60,
+    "title": "Dil Cheer Ke Dekh",
+    "artist": "Kumar Sanu",
+    "videoId": "9f6GhUb-WdM",
+    "cover": "https://img.youtube.com/vi/9f6GhUb-WdM/mqdefault.jpg"
+  },
+  {
+    "id": 61,
+    "title": "Premi Aashiq Aawaara",
+    "artist": "Kumar Sanu",
+    "videoId": "rMbQufI9xQw",
+    "cover": "https://img.youtube.com/vi/rMbQufI9xQw/mqdefault.jpg"
+  },
+  {
+    "id": 62,
+    "title": "Dil Diwana",
+    "artist": "Anuradha Paudwal",
+    "videoId": "Mfeg92XPXik",
+    "cover": "https://img.youtube.com/vi/Mfeg92XPXik/mqdefault.jpg"
+  }
 ];
